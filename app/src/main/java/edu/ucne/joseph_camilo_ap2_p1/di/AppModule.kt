503f589bf940ba7be05b12fc620e83ae6851e65b
Joseph792 <joseph_camilo@ucne.edu.do>
2026-02-10 22:00:55 -0400

    Se creó el modulo de la App

---
+++ app/src/main/java/edu/ucne/joseph_camilo_ap2_p1/di/AppModule.kt
@@ -0,0 +1,11 @@
+package edu.ucne.joseph_camilo_ap2_p1.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+}
