9b19295b13b2ce558fc29d7c829cf7ac6d098b5e
Joseph792 <joseph_camilo@ucne.edu.do>
2026-02-10 22:27:08 -0400

    Agregando lo faltante al modulo de la app

--- app/src/main/java/edu/ucne/joseph_camilo_ap2_p1/di/AppModule.kt
+++ app/src/main/java/edu/ucne/joseph_camilo_ap2_p1/di/AppModule.kt
@@ -1,11 +1,27 @@
 package edu.ucne.joseph_camilo_ap2_p1.di
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.joseph_camilo_ap2_p1.data.local.database.AppDatabase
+import javax.inject.Singleton
 
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext appContext: Context): AppDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            AppDatabase::class.java,
+            "App.Database"
+        ).fallbackToDestructiveMigration()
+            .build()
+    }
 }
