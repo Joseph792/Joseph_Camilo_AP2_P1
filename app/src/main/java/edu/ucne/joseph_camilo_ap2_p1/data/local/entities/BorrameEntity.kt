592795ccb20a59f20cf675211d3bc94e1129e36f
Joseph792 <joseph_camilo@ucne.edu.do>
2026-02-10 21:48:25 -0400

    Se creó BorrameEntity

---
+++ app/src/main/java/edu/ucne/joseph_camilo_ap2_p1/data/local/entities/BorrameEntity.kt
@@ -0,0 +1,11 @@
+package edu.ucne.joseph_camilo_ap2_p1.data.local.entities
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Borrame")
+data class BorrameEntity(
+    @PrimaryKey(autoGenerate = true)
+    val borrameId: Int = 0
+)
