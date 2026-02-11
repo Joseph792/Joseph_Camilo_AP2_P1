3826fc888455d73d83404033c9ee5342a71cbdae
Joseph792 <joseph_camilo@ucne.edu.do>
2026-02-10 22:23:29 -0400

    Se crearon los Screen de List y Edit con su preview

---
+++ app/src/main/java/edu/ucne/joseph_camilo_ap2_p1/presentation/tareas/edit/EditBorrameScreen.kt
@@ -0,0 +1,13 @@
+package edu.ucne.joseph_camilo_ap2_p1.presentation.tareas.edit
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview
+@Composable
+private fun EditBorrameBodyPreview() {
+    MaterialTheme {
+    }
+}
---
+++ app/src/main/java/edu/ucne/joseph_camilo_ap2_p1/presentation/tareas/list/ListBorrameScreen.kt
@@ -0,0 +1,12 @@
+package edu.ucne.joseph_camilo_ap2_p1.presentation.tareas.list
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+private fun ListBorrameBodyPreview() {
+    MaterialTheme {
+    }
+}
