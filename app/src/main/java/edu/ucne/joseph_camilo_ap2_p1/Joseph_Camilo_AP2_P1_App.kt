e0c47c2ac3cc4ccfb1aa046e3d7c1f226db23c8c
Joseph792 <joseph_camilo@ucne.edu.do>
2026-02-10 21:23:13 -0400

    Agregar  android:name en manifest

---
+++ app/src/main/java/edu/ucne/joseph_camilo_ap2_p1/Joseph_Camilo_AP2_P1_App.kt
@@ -0,0 +1,8 @@
+package edu.ucne.joseph_camilo_ap2_p1
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Joseph_Camilo_AP2_P1_App : Application() {
+}
